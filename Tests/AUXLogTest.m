6222718c0b290810c672fb8d23bb7f22a4e4502f
Esad Hajdarevic <esad@esse.at>
2013-10-06 00:45:24 +0200

    Add a test for AUXLog that reads system log via ASL to compare the output

--- Tests/AUXLogTest.m
+++ Tests/AUXLogTest.m
@@ -1,17 +1,51 @@
 #import <XCTest/XCTest.h>
+#import <asl.h>
 #import "AUXLog.h"
 
+static NSString* LastConsoleMessage() {
+    uint64_t since = ([[NSDate date] timeIntervalSince1970] - 2);
+
+    NSMutableArray *consoleLog = [NSMutableArray array];
+
+    aslclient client = asl_open(NULL, NULL, ASL_OPT_STDERR);
+
+    aslmsg query = asl_new(ASL_TYPE_QUERY);
+    asl_set_query(query, ASL_KEY_MSG, NULL, ASL_QUERY_OP_NOT_EQUAL);
+    asl_set_query(query, ASL_KEY_SENDER, "xctest", ASL_QUERY_OP_EQUAL);
+    asl_set_query(query, ASL_KEY_TIME, [[NSString stringWithFormat:@"%" PRIu64, since] UTF8String], ASL_QUERY_OP_GREATER_EQUAL | ASL_QUERY_OP_NUMERIC);
+    aslresponse response = asl_search(client, query);
+
+    asl_free(query);
+
+    aslmsg message;
+    while((message = aslresponse_next(response)))
+    {
+        const char *msg = asl_get(message, ASL_KEY_MSG);
+        [consoleLog addObject:[NSString stringWithCString:msg encoding:NSUTF8StringEncoding]];
+    }
+
+    aslresponse_free(response);
+    asl_close(client);
+
+    return [consoleLog lastObject];
+}
+
 @interface AUXLogTest : XCTestCase
+
 @end
 
 @implementation AUXLogTest
 
 - (void)testLog
 {
-    AUXLog(CGRectMake(1,1,1,1));
-    AUXLog(@"foo");
-    AUXLog(@"Foo %@ + %f", @"bar", 3.4);
-    AUXLog(@"R=%@", AUXLogStringify(CGRectMake(1,1,1,1)));
+    AUXLog(CGRectMake(10,20,320,155));
+    XCTAssertEqualObjects(LastConsoleMessage(), @"{{10, 20}, {320, 155}}");
+
+    AUXLog(CGPointMake(-10,30));
+    XCTAssertEqualObjects(LastConsoleMessage(), @"{-10, 30}");
+
+    AUXLog(CGSizeMake(13,37));
+    XCTAssertEqualObjects(LastConsoleMessage(), @"{13, 37}");
 }
 
 @end
