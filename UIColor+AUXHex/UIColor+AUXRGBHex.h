6886735b7c1579dc322480a4dd00a4f815f1e8b4
Esad Hajdarevic <esad@esse.at>
2013-10-05 21:21:37 +0200

    Add UIColor+AUXRGBHex

---
+++ UIColor+AUXHex/UIColor+AUXRGBHex.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+@interface UIColor(AUXRGBHex)
+
++(UIColor *)auxColorWithRGBHex:(UInt32)hex;
++(UIColor *)auxColorWithRGBHex:(UInt32)hex alpha:(CGFloat)alpha;
+
+@end
---
+++ UIColor+AUXHex/UIColor+AUXRGBHex.m
@@ -0,0 +1,20 @@
+#import "UIColor+AUXRGBHex.h"
+
+@implementation UIColor(RGBHex)
+
++ (UIColor *)auxColorWithRGBHex:(UInt32)hex alpha:(CGFloat)alpha {
+    int r = (hex >> 16) & 0xFF;
+    int g = (hex >> 8) & 0xFF;
+    int b = (hex) & 0xFF;
+
+    return [UIColor colorWithRed:r / 255.0f
+                           green:g / 255.0f
+                            blue:b / 255.0f
+                           alpha:alpha];
+}
+
++ (UIColor *)auxColorWithRGBHex:(UInt32)hex {
+    return [self auxColorWithRGBHex:hex alpha:1.0f];
+}
+
+@end
