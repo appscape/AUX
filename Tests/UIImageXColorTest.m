d1904dc808f6e3639c58fb076a45802cdfeb673a
Esad Hajdarevic <esad@esse.at>
2014-11-19 00:40:53 +0100

    Fix UIImage+XColor test by forcing correct data format

--- Tests/UIImageXColorTest.m
+++ Tests/UIImageXColorTest.m
@@ -12,11 +12,22 @@
     XCTAssertEqualWithAccuracy(image.size.width, 1, FLT_EPSILON,  @"Invalid image width");
     XCTAssertEqualWithAccuracy(image.size.height, 1, FLT_EPSILON, @"Invalid image height");
 
-    const UInt8* data = CFDataGetBytePtr(CGDataProviderCopyData(CGImageGetDataProvider(image.CGImage)));
-    XCTAssertEqual(data[3], (UInt8)(0.6 * 0xff), @"R component wrong");
-    XCTAssertEqual(data[2], (UInt8)(0.4 * 0xff), @"G component wrong");
-    XCTAssertEqual(data[1], (UInt8)(0.2 * 0xff), @"B component wrong");
-    XCTAssertEqual(data[0], (UInt8)(1.0 * 0xff), @"Alpha component wrong");
+    CGImageRef imageRef = image.CGImage;
+
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    unsigned char *pixel = malloc(4);
+    CGContextRef context = CGBitmapContextCreate(pixel, 1, 1, 8, 4, colorSpace, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGColorSpaceRelease(colorSpace);
+    CGContextDrawImage(context, CGRectMake(0, 0, 1, 1), imageRef);
+    CGContextRelease(context);
+
+    const UInt8* data = pixel;
+    XCTAssertEqual(data[0], (UInt8)(0.6 * 0xff), @"R component wrong");
+    XCTAssertEqual(data[1], (UInt8)(0.4 * 0xff), @"G component wrong");
+    XCTAssertEqual(data[2], (UInt8)(0.2 * 0xff), @"B component wrong");
+    XCTAssertEqual(data[3], (UInt8)(1.0 * 0xff), @"Alpha component wrong");
+
+    free(pixel);
 }
 
 @end
